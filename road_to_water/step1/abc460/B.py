694883bc10ba3790701821286537660b274719d9
rnd3088 <kyjobkyo@gmail.com>
2026-06-02 16:59:09 +0900

    done

--- road_to_water/step1/abc460/B.py
+++ road_to_water/step1/abc460/B.py
@@ -3,15 +3,12 @@ T = int(input())
 for _ in range(T):
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
 
-    dx = x1 - x2
-    dy = y1 - y2
+    d2 = (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-    dist2 = dx * dx + dy * dy
+    min_d = abs(r1 - r2)
+    max_d = r1 + r2
 
-    inner = (r1 - r2) ** 2
-    outer = (r1 + r2) ** 2
-
-    if inner <= dist2 <= outer:
+    if min_d ** 2 <= d2 <= max_d ** 2:
         print("Yes")
     else:
         print("No")
